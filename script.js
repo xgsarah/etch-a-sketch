c807428fbefc9df01bd03cc94e93295cb479ab05
xgsarah <sjbquimpo@gmail.com>
2024-02-03 21:26:12 +0800

    create 16x16 square divs

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+function createGrid(x, y) {
+  const container = document.querySelector('.container');
+  for (let i = 0; i < x * y; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'item');
+    container.appendChild(div);
+  }
+}
+
+createGrid(16, 16);
